522d1d60b31423ddebc7b41ed2cd63f31e1faa57
Mitchell <Hmitchell.hall@gmail.com>
2021-08-10 14:10:12 -0500

    commit tree 416deae2471d36480325d65edcb892c4faf9a369

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(param = "roller-skate"){
+    return `This Saturday, I want to ${param}!`
+}
+const mondayWork = function(p = "go to the office"){
+    return `This Monday, I will ${p}.`
+}
+function wrapAdjective(wrapper = "*"){
+    return function(inner = "special"){
+        return `You are ${wrapper}${inner}${wrapper}!`
+    }
+    }
+
+    
